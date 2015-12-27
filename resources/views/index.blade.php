d3acfa0681ac0275549a2d594657e0c42d85d386
Gong, Yi-Jhong <yijhong@hotmail.com.tw>
2015-12-27 15:11:26 +0800

    Add custom validation messages and update the index.blade.php content

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,52 @@
 @extends('layout.general')
+
+@section('content')
+
+    <div class="row marketing">
+        <div style="text-align: center;">
+            <h2>請選擇您的組別：</h2>
+        </div>
+    </div>
+    <br>
+    <div class="row marketing" style="text-align: center;">
+        <table border="0" width="100%" style="vertical-align: top;">
+            <tr>
+                <td width="2%"></td>
+                <td width="47%">
+                    <h2 class="ui header">國小組</h2>
+                    <br>
+                    <p><a class="btn btn-lg btn-success" href="regPrimary" role="button">報名</a></p>
+                </td>
+                <td width="2%">  </td>
+                <td width="47%">
+                    <h2 class="ui header">國中組</h2>
+                    <br>
+                    <p><a class="btn btn-lg btn-success" href="regJunior" role="button">報名</a></p>
+                </td>
+                <td width="2%">  </td>
+            </tr>
+        </table>
+        <br><br>
+        <table border="0" width="100%" style="vertical-align: top;">
+            <tr>
+                <td width="2%"></td>
+                <td width="96%">
+                    <p><span class="text-danger" style="text-align: center;">若有相關帳戶問題請連絡 resttc@ccu.edu.tw</span></p>
+                    <div class="panel panel-warning">
+                        <div class="panel-heading">
+                            <h3 class="panel-title"><span class="glyphicon glyphicon-time" aria-hidden="true"></span> 報名作業時程</h3>
+                        </div>
+                        <div class="panel-body">
+                            <br>
+                            嘟嚕嚕～敬請期待
+                            <br><br>
+                        </div>
+                    </div>
+                </td>
+                <td width="2%">  </td>
+            </tr>
+        </table>
+
+    </div>
+
+    @stop
--- resources/views/register/register.blade.php
+++ resources/views/register/register.blade.php
@@ -7,35 +7,39 @@
             <div class="form-group">
                 <div class="col-lg-6">
                     <div class="panel panel-default">
-                        <div class="panel-heading">個人資料</div>
+                        <div class="panel-heading"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> 個人資料</div>
                         <div class="panel-body">
                             @if($type == 'Primary')
-                                <div class="alert alert-warning">您現在填寫的是 國小教師 申請網頁 <a href="regJunior">帶我去國中申請頁面</a> </div>
+                                <div class="alert alert-warning"><span class="glyphicon glyphicon-asterisk" aria-hidden="true"></span> 您現在填寫的是 國小教師 申請網頁 <a href="regJunior">帶我去國中申請頁面</a> </div>
                             @elseif($type == 'Junior')
-                                <div class="alert alert-success">您現在填寫的是 國中教師 申請網頁 <a href="regPrimary">帶我去國小申請頁面</a></div>
+                                <div class="alert alert-success"><span class="glyphicon glyphicon-asterisk" aria-hidden="true"></span> 您現在填寫的是 國中教師 申請網頁 <a href="regPrimary">帶我去國小申請頁面</a></div>
                             @endif
                             <div class="form-group">
                                 {!! Form::label('email', 'E-Mail 帳號（您在本系統的登入帳號）') !!}
                                 {!! Form::text('email', null, ['placeholder' => 'Email', 'class' => 'form-control']) !!}
                                 <h5 class="text-muted">＊請輸入有效的E-Mail帳號，以便認證及啟用帳號</h5>
-
+                                @if ($errors->has('email')) <h5 class="text-danger">{{ $errors->first('email') }}</h5> @endif
                             </div>
                             <div class="form-group">
                                 {!! Form::label('pwd', '密碼 （至少八個字元）') !!}
                                 {!! Form::password('pwd', ['placeholder' => '密碼', 'class' => 'form-control']) !!}
+                                @if ($errors->has('pwd')) <h5 class="text-danger">{{ $errors->first('pwd') }}</h5> @endif
                             </div>
                             <div class="form-group">
                                 {!! Form::label('cmf_pwd', '確認密碼') !!}
                                 {!! Form::password('cmf_pwd', ['placeholder' => '確認密碼', 'class' => 'form-control']) !!}
+                                @if ($errors->has('cmf_pwd')) <h5 class="text-danger">{{ $errors->first('cmf_pwd') }}</h5> @endif
                             </div>
                             <div class="form-group">
                                 {!! Form::label('name', '姓名') !!}
                                 {!! Form::text('name', null, ['placeholder' => '請填寫您的大名', 'class' => 'form-control']) !!}
+                                @if ($errors->has('name')) <h5 class="text-danger">{{ $errors->first('name') }}</h5> @endif
                             </div>
                             <div class="form-group">
                                 {!! Form::label('pid', '身分證字號') !!}
                                 {!! Form::text('pid', null, ['placeholder' => '請填寫您的身分證字號', 'class' => 'form-control']) !!}
                                 <h5 class="text-muted">＊首字應為大寫；請確實填寫，以利未來核對身份之用</h5>
+                                @if ($errors->has('pid')) <h5 class="text-danger">{{ $errors->first('pid') }}</h5> @endif
                             </div>
                             <div class="form-group">
                                 {!! Form::label('gender', '性別') !!}
@@ -43,6 +47,7 @@
                                 {!! Form::radio('gender', '0') !!}
                                 {!! Form::label('gender', '女') !!}
                                 {!! Form::radio('gender', '1') !!}
+                                @if ($errors->has('gender')) <h5 class="text-danger">{{ $errors->first('gender') }}</h5> @endif
                             </div>
                             <div class="form-group">
                                 {!! Form::label('tc_class', '教師身份') !!}
@@ -54,11 +59,13 @@
                                 {!! Form::radio('tc_class', '3') !!}
                                 {!! Form::label('tc_class', '其他') !!}
                                 {!! Form::radio('tc_class', '4') !!}
+                                @if ($errors->has('tc_class')) <h5 class="text-danger">{{ $errors->first('tc_class') }}</h5> @endif
                             </div>
                             <div class="form-group">
                                 {!! Form::label('phone', '聯絡電話（純數字無連字符）') !!}
                                 {!! Form::text('phone', null, ['placeholder' => '請填寫您的聯絡電話', 'class' => 'form-control']) !!}
                                 <h5 class="text-muted">＊請輸入有效電話號碼，以便認證及啟用帳號</h5>
+                                @if ($errors->has('phone')) <h5 class="text-danger">{{ $errors->first('phone') }}</h5> @endif
                             </div>
                         </div>
                     </div>
@@ -107,7 +114,6 @@
         </div>
 
     {!! Form::close() !!}
-    {{ var_dump($errors) }}
 
     @stop
 
