6181907a84bcb78e13f1cc8bb3bb5b746e6b6e6e
onemid <onemid85@gmail.com>
2015-12-30 03:16:10 +0800

    1. Add the Login Interface in generalLogin pages

--- resources/views/auth/generalLogin.blade.php
+++ resources/views/auth/generalLogin.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
 
+    {!! Form::open(['url' => 'generalLogin']) !!}
+    <div class="row">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-4 text-center">
 
+            <div class="form-group">
+                {!! Form::label('email', 'E-Mail帳號') !!}
+                {!! Form::text('email', null, ['placeholder' => '您的電子郵件帳號', 'class' => 'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('pwd', '密碼') !!}
+                {!! Form::password('pwd', ['placeholder' => '您的密碼', 'class' => 'form-control']) !!}
+            </div>
+
+        </div>
+        <div class="col-lg-4"></div>
+    </div>
 
     @stop
